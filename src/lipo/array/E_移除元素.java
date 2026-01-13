76f68970d5beb91c224dfbca1b12ca63c794cf10
Alexlipo <1798527554@qq.com>
2026-01-13 21:05:29 +0800

    移除元素

---
+++ src/lipo/array/E_移除元素.java
@@ -0,0 +1,19 @@
+package lipo.array;
+
+/**
+ * https://leetcode.cn/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class E_移除元素 {
+
+    public int removeElement(int[] nums, int val) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[p++] = nums[i];
+            }
+        }
+        return p;
+
+    }
+
+}
