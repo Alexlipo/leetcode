26130956795787cf93ecce45e2454526b9f8c855
Alexlipo <1798527554@qq.com>
2026-01-13 21:05:19 +0800

    合并两个有序数组

--- src/lipo/array/E_合并两个有序数组.java
+++ src/lipo/array/E_合并两个有序数组.java
@@ -6,8 +6,11 @@ package lipo.array;
 public class E_合并两个有序数组 {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // nums1 有效尾
         int i = m - 1;
+        // nums2 尾
         int j = n - 1;
+        // nums1 总尾
         int tail = m + n - 1;
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
@@ -16,6 +19,7 @@ public class E_合并两个有序数组 {
                 nums1[tail--] = nums2[j--];
             }
         }
+        // nums2 还有剩余，拷贝进 nums1
         while (j >= 0) {
             nums1[tail--] = nums2[j--];
         }
