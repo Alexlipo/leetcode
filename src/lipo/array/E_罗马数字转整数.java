1c9e087806c936b0963905e91462678f83c2e17d
Alexlipo <1798527554@qq.com>
2026-01-16 20:15:25 +0800

    罗马数字转整数

---
+++ src/lipo/array/E_罗马数字转整数.java
@@ -0,0 +1,38 @@
+package lipo.array;
+
+/**
+ * https://leetcode.cn/problems/roman-to-integer/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class E_罗马数字转整数 {
+
+    public int romanToInt(String s) {
+        int n = s.length();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cur = value(s.charAt(i));
+
+            // 如果后面还有字符，并且当前 < 后一个 → 减
+            if (i + 1 < n && cur < value(s.charAt(i + 1))) {
+                sum -= cur;
+            } else {
+                sum += cur;
+            }
+        }
+        return sum;
+    }
+
+    private int value(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+
+}
