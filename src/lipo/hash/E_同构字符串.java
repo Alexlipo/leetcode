5eedd6acaca9a6e66759dac70eb21fb4a51ffa4b
Alexlipo <1798527554@qq.com>
2026-01-28 20:51:52 +0800

    同构字符串

---
+++ src/lipo/hash/E_同构字符串.java
@@ -0,0 +1,23 @@
+package lipo.hash;
+
+/**
+ * https://leetcode.cn/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class E_同构字符串 {
+
+    public boolean isIsomorphic(String s, String t) {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapS[c1] != mapT[c2]) return false;
+
+            mapS[c1] = i + 1;
+            mapT[c2] = i + 1;
+        }
+        return true;
+    }
+}
